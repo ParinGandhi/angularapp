620795ec3d721e7ac3fad598c9564d0ed2bc769c
Parin Gandhi <parin.a.gandhi@gmail.com>
2018-01-08 17:40:07 -0500

    Added new component

---
+++ src/app/components/sandbox.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'sandbox', // name of html tag this component will control
+    // using back ticks instread of single quotes allows entering of template strings spanning multiple lines
+    template: `<h1>Hello World</h1>`
+})
+
+export class SandboxComponent {
+
+}
