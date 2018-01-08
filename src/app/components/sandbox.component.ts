45a33ba33dc57a66e4f4ac4364ed0de3b8d62884
Parin Gandhi <parin.a.gandhi@gmail.com>
2018-01-08 18:55:12 -0500

    Setting objects using an externalized interface

--- src/app/components/sandbox.component.ts
+++ src/app/components/sandbox.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Customer } from './Customer';
 
 @Component({
     selector: 'sandbox',
-    template: `<h1>Hellow World</h1>`
+    template: `<h1>Hello World</h1>`
 })
 
 export class SandboxComponent {
+    customer: Customer;
+    customers: Customer[];
 
+    constructor() {
+        this.customer = {
+            id: 1,
+            name: 'John Doe',
+            email: 'john@gmail.com'
+        },
+            {
+                id: 2,
+                name: 'Jane Doe',
+                email: 'jane@gmail.com'
+            },
+            {
+                id: 3,
+                name: 'Steve Sith',
+                email: 'smith@gmail.com'
+            }
+    }
 }
+
