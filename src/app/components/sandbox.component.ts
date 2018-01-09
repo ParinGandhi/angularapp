b5930a6667322923e48f61ef665f3dd8d50383ce
Parin Gandhi <parin.a.gandhi@gmail.com>
2018-01-08 23:14:37 -0500

    Manipulating components by events

--- src/app/components/sandbox.component.ts
+++ src/app/components/sandbox.component.ts
@@ -2,9 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'sandbox',
-    template: `<h1>Hello World</h1>`
+    template: `
+        <h1>Hello World</h1>
+        <button (click)="changeValue()">Change value</button>
+        <h1>{{text}}</h1>
+        <div *ngIf="value">
+            <h1>{{text}}</h1>
+        </div>
+    `
 })
 
 export class SandboxComponent {
+    text: string = 'Hello World';
+    value: boolean = true;
 
+    changeValue() {
+        this.text = 'Goodbye World';
+        this.value = !this.value;
+    }
 }
