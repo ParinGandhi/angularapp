01a9bb8d82f31b6cf0c799ceabe578c242f890b8
Parin Gandhi <parin.a.gandhi@gmail.com>
2018-01-08 23:07:14 -0500

    Playing aorund with different mouse events

--- src/app/components/sandbox.component.ts
+++ src/app/components/sandbox.component.ts
@@ -2,9 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'sandbox',
-    template: `<h1>Hello World</h1>`
+    template: `
+        <h1>Hello World</h1>
+        <button id="btn" (click)="fireEvent($event)">Click event</button>
+        <br>
+        <button id="btn" (mouseover)="fireEvent($event)">Mouseover event</button>
+        <br>
+        <button id="btn" (mousedown)="fireEvent($event)">Mousedown event</button>
+        <br>
+        <button id="btn" (mouseup)="fireEvent($event)">Mouseup event</button>
+        <br>
+        <button id="btn" (dblclick)="fireEvent($event)">Double click event</button>
+        <br>
+        <button id="btn" (drag)="fireEvent($event)">Drag event</button>
+        <br>
+        <button id="btn" (dragover)="fireEvent($event)">Drag over event</button>
+    `
 })
 
 export class SandboxComponent {
-
+    fireEvent(event) {
+        console.log(event.type);
+    }
 }
