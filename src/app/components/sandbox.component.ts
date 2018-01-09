2cc7ca0ebbb7693569af328f50fcac9f550d7820
Parin Gandhi <parin.a.gandhi@gmail.com>
2018-01-08 20:16:08 -0500

    Dynamically setting styles using ngStyle directive

--- src/app/components/sandbox.component.ts
+++ src/app/components/sandbox.component.ts
@@ -2,9 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'sandbox',
-    template: `<h1>Hello World</h1>`
+    template: `
+        <h1>Hello World</h1>
+        <div [style.font-size]="isSpecial ? 'x-large' : 'smaller'">Font size depends on isSpecial</div>
+        <div [ngStyle]="currentStyles">This div is initially saveable and special</div>
+    `
 })
 
 export class SandboxComponent {
+    isSpecial: boolean = true;
+    canSave: boolean = true;
+    currentStyles = {};
 
+    constructor() {
+        this.setCurrentStyles();
+    }
+
+    setCurrentStyles() {
+        this.currentStyles = {
+            'font-style': this.canSave ? 'italic' : 'normal';
+            'font-size': this.isSpecial ? '24px' : '12px';
+        }
+    }
 }
